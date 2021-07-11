b4dfa0c5b35839c68c63a22d790314a79c48df3e
Jumail Mundekkat <mundekkat@hotmail.com>
2021-07-11 18:32:57 +1000

    Export methods and classes from bot.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from "./constants/log_levels.js";
 export * from "./cloudflare_bypass.js";
 export * from "./dependency.js";
 export * from "./imm.js";
+export * from "./bot.js";
 export * from "./scrollable.js"
 export * from "./timer.js";
 export * from "./logger.js"
